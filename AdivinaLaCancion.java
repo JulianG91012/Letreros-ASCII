6efb86e9a52af5ec7462e45c9fa84079b3161245
Andres <andres.saldarriagaa@upb.edu.co>
2021-05-06 16:15:54 -0500

    Cambio nombre y funciones menu

--- AdivinaLaCancion.java
+++ AdivinaLaCancion.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
-public class ADIVINA_LA_CANCION {
+import java.util.Scanner;	
+public class AdivinaLaCancion {
 	public static void displayVacio(int n,int m){
 		for (int i=0;i<n;i++) 
 		{
@@ -27,15 +28,11 @@ public static void imprimirDisplay(int n,int m){
 			System.out.println();
 		}
 	}
-
-
-
-
-
-
-
 	public static void main(String[] args) {
-		
+		int nombre_cancion = 1;
+		int creditos = 40;
+		int rondas = 0;
+		Scanner ingreso = new Scanner(System.in);
 
 		System.out.println();
 				System.out.println("Bienvenido a Adivina la Canción, tienes las siguientes opciones para comenzar:");
@@ -44,10 +41,48 @@ public static void imprimirDisplay(int n,int m){
 				System.out.println("3. Reglas del juego");
 				System.out.println("4. Tienda");
 				System.out.println("5. Realizar Tutorial");
-				System.out.println("6. Salir");
+				System.out.println("6. Salir. \n");
 
+				int opcion = ingreso.nextInt();
 
-		
+				if (opcion == 1){
+					System.out.println("Cuantas canciones deseas adivinar? (1-22) ");
+					rondas = ingreso.nextInt();
+
+					if (rondas == 1) {
+						//reproduce la primer cancion//
+						System.out.println("¿Como se llama esta cancion? \n");
+
+						int respuesta = ingreso.nextInt();
+
+					if (respuesta == nombre_cancion) {
+						System.out.println("Has acertado!");
+						creditos = creditos + 10;
+						System.out.println("Tus creditos ahora son: " + creditos);
+					} 
+					else{
+						System.out.println("No has acertado!");
+						creditos = creditos - 5;
+					}
+						
+					}
+					//se reproduce la cancion 
+					 
+				}
+
+				else if (opcion == 2) {
+					System.out.println("Tu numero de creditos es: " + creditos);
+				
+				}
+				else if (opcion == 3) {
+					System.out.println("Las reglas del juego son: \n 1.Solamente se tienen 3 intentos para adivinar la cancion. \n 2.si no se tienen los creditos no podra continuar. \n 3. El nombre le cancion debe ser escrito completo y con buena ortografia \n 4. se puede retirar en cualquier momento con los creditos que al final le daran un puntaje.");
+
+					
+				}
+				else if (opcion == 4) {
+					System.out.println("La tienda no se encuentra disponible en este momento.");
+					
+				}
 
 		
 
